cb27d5407c11fec0b3de9080954c9b64fb5ceca4
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-01-14 17:22:45 +0500

    Revert 59915b2..da53147
    This rolls back to commit 59915b2b9713e8edcdc2a5fe2f9bb9a017e4300d.

--- WowAttendance/WowAttendanceTests/WowAttendanceTests.swift
+++ WowAttendance/WowAttendanceTests/WowAttendanceTests.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 import XCTest
-import WowAttendance
-
 
 class WowAttendanceTests: XCTestCase {
     
@@ -33,17 +31,6 @@ class WowAttendanceTests: XCTestCase {
         self.measureBlock() {
             // Put the code you want to measure the time of here.
         }
-    }
-    
-    // we can't do much  without a view on our root View Controller
-    func testViewDidLoad()
-    {
-        // we only have access to this if we import our project above
-        let v = ViewController()
-        
-        
-        // assert that the ViewController.view is not nil
-        XCTAssertNotNil(v.view, "View Did Not load")
     }
     
 }
